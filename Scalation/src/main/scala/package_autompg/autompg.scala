54c041f9442e6ecd4ef4def794b5a7b5d55dae9d
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-02-17 17:03:17 -0500

    implemented mean-imputation

--- Scalation/src/main/scala/package_autompg/autompg.scala
+++ Scalation/src/main/scala/package_autompg/autompg.scala
@@ -117,27 +117,28 @@ object autompg extends App {
 	}
 	
 	def main(){
-		banner (" Select dataset: \n\t1. Auto MPG \n\t2. Lorem Ipsum \n\t11. Enter: /correct/path/to/data/csv")
+		banner (" Select dataset: \n\t1. Auto MPG \n\t2. Lorem Ipsum \n\t11. For other datasets, enter: /correct/path/to/data/csv")
 		val choice	 = scala.io.StdIn.readLine()
 /*		Solve bug for choice = 11			*/
-//		val filename = if(choice != "11") {choice + ".csv"} else {scala.io.StdIn.readLine()}
-		val filename = if(choice != "11") {"autompg.csv"} else {scala.io.StdIn.readLine()}
+		val filename = if(choice != "11") {choice + ".csv"} else {scala.io.StdIn.readLine()}
 		val dataset = Relation (filename, "dataset", null, -1)
 		val column_names = dataset.colNames
 		val num_cols = dataset.cols
 
-/* Experiments for Mean Imputation		
+		// Mean Imputation 		
 		for(i <- 0 to (num_cols - 1)){
 			val selected = dataset.sigmaS(column_names(i), (x) => x!="")
-			//val mean_col = selected.mean
-			//val imputed = ImputeMean.impute(dataset.column(column_names(i)))
-			//dataset.update(column_names(i), val_mean, "") 
-		} */
-		/*
+			val v_selected = selected.toVectorS(column_names(i))
+			val v_seld = v_selected.map((x) => x.toDouble)
+			val mean_col = (v_seld.sum) / selected.count(column_names(i))
+			println(mean_col)
+			dataset.update(column_names(i), mean_col.toString(), "") 
+		} 
+		
 		banner ("Extracting X matrix and Y vector: ")
 		val (x_initial, y) = dataset.toMatriDD(1 to 6, 0)
 		val x = VectorD.one (x_initial.dim1) +^: x_initial
-		quad_regression (x, y) */
+		quad_regression (x, y)
 	}
 
 	main()
