fb75d3d168206e418f2931640266fb883051693b
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-02-17 20:55:32 -0500

    added model selection functionality

--- Scalation/src/main/scala/package_autompg/autompg.scala
+++ Scalation/src/main/scala/package_autompg/autompg.scala
@@ -2,7 +2,6 @@
 
 package package_autompg
 import scalation.columnar_db.Relation
-import scalation.columnar_db.ImputeMean
 import scalation.util.banner
 import scala.collection.mutable.Set
 import scalation.linalgebra._
@@ -21,9 +20,22 @@ class Exception1{
 	}
 }	
 			
-
-
 object autompg extends App {
+	/* def cross_validation(x: MatriD, y: VectorD)
+	{
+		val k = 10
+		val m = x.dim1
+		val indices = VectorI(0 until m).split(k)
+		
+		for (idx <- indices) {
+			val idx_array = idx.toArray
+			val XTe = x(idx)
+			val YTe = y(idx)
+			val XTr = x.selectRowsEx(idx_array)
+			val YTr = y.selectEx(idx_array)
+		}
+	} */
+
 	def regression_sim (x: MatriD, y: VectorD)
 	{
 		banner ("Implementing Simple Regression... ")
@@ -129,6 +141,20 @@ object autompg extends App {
 		banner ("Successfully implemented Quadratic Regression!")		
 	}
 	
+	def lasso_regression(x: MatriD, y: VectorD)
+	{
+		banner ("Implementing Lasso Regression...")
+		
+		banner ("Successfully implemented Lasso Regression!")
+	}
+	
+	def response_surface (x: MatriD, y: VectorD)
+	{
+		banner ("Implementing Response Surface... ")
+		
+		banner ("Successfully implemented Response Surface!")
+	}
+	
 	def main(){
 		banner (" Select dataset: \n\t1. Auto MPG \n\t2. Lorem Ipsum \n\t11. For other datasets, enter: /correct/path/to/data/csv")
 		val choice	 = scala.io.StdIn.readLine()
@@ -153,10 +179,30 @@ object autompg extends App {
 			dataset.update(column_names(i), mean_col.toString(), "") 
 		} 
 		
-		banner ("Extracting X matrix and Y vector: ")
-		val (x_initial, y) = dataset.toMatriDD(1 until num_cols, 0)
-		val x = VectorD.one (x_initial.dim1) +^: x_initial
-		ridge_regression (x, y)
+		banner ("Select model:\n\t1. Simple Regression \n\t2. Regression WLS \n\t3. Quadratic Regression \n\t 4. Ridge Regression \n\t 5. Lasso Regression \n\t 6. Response Surface")
+		val model = scala.io.StdIn.readLine()
+		if (model == "1") {
+			val (x_initial, y) = dataset.toMatriDD(1 until num_cols, 0)
+			val x = VectorD.one (x_initial.dim1) +^: x_initial
+			regression_sim(x, y)
+		} else if (model == "2") {
+			val (x_initial, y) = dataset.toMatriDD(1 until num_cols, 0)
+			val x = VectorD.one (x_initial.dim1) +^: x_initial
+			regression_WLS(x, y)
+		} else if (model == "3") {
+			val (x_initial, y) = dataset.toMatriDD(1 until num_cols, 0)
+			val x = VectorD.one (x_initial.dim1) +^: x_initial
+			quad_regression(x, y)
+		} else if (model == "4") {
+			val (x, y) = dataset.toMatriDD(1 until num_cols, 0)
+			ridge_regression(x, y)
+		} else if (model == "5") {
+			val (x, y) = dataset.toMatriDD(1 until num_cols, 0)
+			lasso_regression(x, y)
+		} else if (model == "6") {
+			val (x, y) = dataset.toMatriDD(1 until num_cols, 0)
+			response_surface(x, y)
+		}
 	}
 
 	main()
