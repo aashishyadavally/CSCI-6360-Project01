66fd69d4f5ddd209a7985c53cbf45dfdf996b988
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-02-24 00:02:57 -0500

    added cross validation function with model choice

--- R/forward_selection.R
+++ R/forward_selection.R
@@ -14,12 +14,31 @@ get_criterion <- function(data.frame) {
 	return(rsq.vector)
 }
 
+cross.validation <- function(data.frame, choice) {
+	train.control <-trainControl(method = "cv", number = 10)
+	if(choice == "1") {
+		cv.results <- train(mpg ~ ., data=data.frame, method="lm", trControl= train.control)	
+	} else if(choice == "2") {
+		cv.results <- train(mpg ~ ., data=data.frame, method="ridge", trControl= train.control)
+	} else if(choice == "3") {
+		cv.results <- train(mpg ~ ., data=data.frame, method="lasso", trControl= train.control)
+	} else if(choice == "4") {
+		cv.results <- train(mpg ~ ., data=data.frame, method="lm", trControl= train.control)  # The dataframe will be different for Quad Regression
+	} else if(choice == "5") {
+		cv.results <- train(mpg ~ ., data=data.frame, method="lm", trControl= train.control) # The dataframe will be different for Response Surface
+	} else {
+		print("Invalid choice.")
+	}
+	return(100*cv.results$results$Rsquared)
+}
+
 main <- function() {
 	file <- read.table("D:/Spring2019/DataScienceII/Projects/CSCI-6360-Project01/data/1.csv", header=TRUE, sep=",")
    	file <- data.frame(sapply(file, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x)))  # Mean Imputation
 	column.names <- colnames(file) 
 	y.column.name <- column.names[1]
 	x.column.names <- column.names[2:length(column.names)]
+	model.choice <- readline(prompt="1. Linear Regression\n2. Ridge Regression\n3. Lasso Regression\n4. Quad Regression\n5. Response Surface\n\n\tChoose model: ")
 
 	# Forward Selection
 	fs.columns <- c(y.column.name) # Forward selection column vector
@@ -40,13 +59,17 @@ main <- function() {
 		}
 		min.index <- which.min(model.rss.values) # Column name with minimum RSS value
 		fs.columns <- append(fs.columns, x.column.names[min.index]) # Forward selection column vector
-		cv.data.frame <- file[,fs.columns]
-		train.control <-trainControl(method = "cv", number = 10)
-		cross.validation <- train(mpg ~ ., data=cv.data.frame, method="lm", trControl= train.control)
 		x.column.names <- x.column.names[x.column.names != x.column.names[min.index]] # Removing 'min.index' from column names vector
 		adj.rSq <- append(adj.rSq, model.adj.rsq.values[min.index]) # Adding adjusted rSq value for column with maximum critrion
 		rSq <- append(rSq, model.rsq.values[min.index]) # Adding rSq value for column with maximum criterion
-		rSqCv <- append(rSqCv, 100*cross.validation$results$Rsquared) # Adding rSqCV value for forward selection vector
+		if(model.choice == "4") {
+
+		} else if(model.choice == "5") {
+
+		}
+		else 
+		cv.data.frame <- file[,fs.columns] 
+		rSqCv <- append(rSqCv, cross.validation(cv.data.frame, model.choice)) # Adding rSqCV value for forward selection vector
 	}
 	#Plotting rSq and rSqCV
 	plot(rSq, type = 'l', col = 'red', main = "Linear Regression", ylab = "Percentage", ylim = c(0,100))
